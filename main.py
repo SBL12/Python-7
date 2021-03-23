09c80036dd259803288d57ccc7a3cc733e0b12cf
Suyash L <suyashblal@gmail.com>
2021-03-23 21:34:11 +0000

    Added formating Functions

---
+++ main.py
@@ -0,0 +1,28 @@
+
+
+sentece = "Hello, World welcome!"
+print(sentece.lower())
+
+sen2 = "I LOVE THIS CLASS!!!"
+print(sen2.upper())
+
+sen3= "I like python."
+print(sen3.upper())
+
+lenstr = len("My name is Suyash")
+print(lenstr)
+
+str4 = "I am learing Python and Javascript"
+check = "Javascript" in str4
+print(check)
+
+
+place = input("What is your favorite place to visit?")
+print(f"Hello, Your favorite place is {place}")
+
+age = input('How old are you?')
+print("Hello, you are {} years old!" .format(age))
+
+name = "Suyash"
+age = "11"
+print("My name is {0}, I'am {1}".format(name,age))
